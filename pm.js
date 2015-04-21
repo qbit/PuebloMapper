f472b209566ef00a4fa143dda1bc8e901cb82276
Aaron Bieber <aaron@bolddaemon.com>
2015-04-21 15:03:05 -0600

    hello leaflet

--- pm.js
+++ pm.js
@@ -1,20 +1,8 @@
-var padUrl = 'http://maps.co.pueblo.co.us/outside/rest/services/pueblo_county_parcels_bld_footprints/MapServer';
 var geocoder = new google.maps.Geocoder();
-var vectorSource = new ol.source.Vector();
-var vectorLayer = new ol.layer.Vector({
-    source: vectorSource,
-    style: getStyle
-});
-var utfGridSource = new ol.source.TileUTFGrid({
-    //url: "http://wms.deftly.net/utfgrid_water_wells/{z}/{x}/{y}.json"
-    url: "http://wms.deftly.net/utfgrid_water_wells/0/0/0.json"
-});
-var utfGridLayer = new ol.layer.Tile({source: utfGridSource});
-
 function prettyUrl(key, val, attrs) {
     key = key || val;
     if (val && val !== "") {
-	return $('<a>').text(key).attr('href', val);
+	return $('<a>').text(key).attr('href', val).attr('target', "_blank");
     }
 }
 
@@ -31,12 +19,57 @@ function prettyMoney(key, val, attrs) {
     return ret;
 }
 
+var wellMap = {
+    receipt:	"Receipt: ",
+    aquifer1:	"Aquifer: ",
+    welldepth:	"Well Depth: ",
+    permit:	"Permit: "
+};
+
+var wellKeyMap = {
+    receipt: function(p) {
+	return $('<a>').text(p)
+	    .attr('href', 'http://www.dwr.state.co.us/WellPermitSearch/View.aspx?receipt=' + p)
+	    .attr('target', '_blank');
+    }
+};
+
+function prettyWell(well) {
+    var t = $('<table>'), k, v, i, l, r = [];
+
+    t.addClass('table-striped');
+
+    for (k in well) {
+	if (well[k]) {
+	    r.push({key: k, val: well[k]});
+	}
+    }
+    r = r.sort();
+
+    for (i = 0, l = r.length; i < l; i++) {
+	var tr = $('<tr>');
+	var rr = r[i];
+	for (k in rr) {
+	    if (k === 'key') {
+		tr.append($('<td>').text(wellMap[rr.key]));
+	    } else {
+		if (wellKeyMap[rr.key]) {
+		    rr.val = wellKeyMap[rr.key](rr.val);
+		}
+		tr.append($('<td>').html(rr.val));
+	    }
+	}
+	t.append(tr);
+    }
+    return t;
+}
+
 var prettyMap = {
     "Parcel Number": "PAR_NUM",
 
     "break0": "Owner Info:",
 
-    "Owner": ["OwnerOverflow", "SubOnwer1", "SubOwner2"],
+    "Owner": ["Owner", "OwnerOverflow", "SubOnwer1", "SubOwner2"],
     "Owner Address": ["OwnerStreetAddress", "OwnerCity", "OwnerState", "OwnerZip", "OwnerCountry"],
     "Legal Description": "LegalDescription",
 
@@ -84,179 +117,14 @@ var prettyMap = {
     "Zoning": "Zoning"
 };
 
-var agisSrc = new ol.source.TileArcGISRest({
-    url: padUrl,
-    attributions: [
-	new ol.Attribution({
-	    html: 'Pueblo County ' +
-		'<a href="http://maps.pueblo.org">Maps</a>'
-	})
-    ]
-});
-
-var deftXYZsrc = new ol.source.XYZ({
-    url: 'http://wms.deftly.net/water_wells/{z}/{x}/{y}.png',
-    attributions: [
-	new ol.Attribution({
-	    html: '<a href="http://water.state.co.us/Home/Pages/default.aspx">CDNR</a>'
-	})
-    ]
-});
-
-var layers = [
-    new ol.layer.Tile({
-	style: 'OSM',
-	visible: false,
-	source: new ol.source.OSM()
-    }),
-    new ol.layer.Tile({
-	style: 'Road',
-	visible: false,
-	source: new ol.source.MapQuest({layer: 'osm'})
-    }),
-    new ol.layer.Tile({
-	style: 'Satellite',
-	visible: false,
-	source: new ol.source.BingMaps({key: 'Aq7e23Eva6DtRFf1iZPllRqUNjHM-zN7ap_TegK6IUm9HuO7a8KeeeiU3b10tFr9', imagerySet: 'Aerial'})
-    }),
-    new ol.layer.Group({
-	style: 'SatWithLabels',
-	visible: true,
-	layers: [
-	    new ol.layer.Tile({
-		source: new ol.source.BingMaps({key: 'Aq7e23Eva6DtRFf1iZPllRqUNjHM-zN7ap_TegK6IUm9HuO7a8KeeeiU3b10tFr9', imagerySet: 'AerialWithLabels'})
-	    }),
-	    new ol.layer.Tile({
-		source: new ol.source.MapQuest({layer: 'hyb'})
-	    })
-	]
-    }),
-    new ol.layer.Group({
-	visible: true,
-	style: 'AlwaysShow',
-	layers: [
-	    new ol.layer.Tile({
-		extent: [-13884991, 2870341, -7455066, 6338219],
-		visible: true,
-		source: agisSrc
-	    }),
-	    new ol.layer.Tile({
-		extent: [-13884991, 2870341, -7455066, 6338219],
-		visible: true,
-		source: deftXYZsrc
-	    }),
-	    vectorLayer,
-	    utfGridLayer
-	]
-    })
-];
-
-var styles = {
-    'Point': [new ol.style.Style({
-	image: new ol.style.Circle({
-	    radius: 5,
-	    fill: new ol.style.Fill({color: 'red'}),
-	    stroke: new ol.style.Stroke({color: 'red', width: 1})
-	})
-    })],
-    'MultiPoint': [new ol.style.Style({
-	image: new ol.style.Circle({
-	    radius: 1,
-	    fill: new ol.style.Fill({color: 'red'}),
-	    stroke: new ol.style.Stroke({color: 'red', width: 3})
-	}),
-	fill: new ol.style.Fill({color: 'rgba(255,255,255,0.4)'}),
-	stroke: new ol.style.Stroke({color: 'green', width: 1})
-    })]
-};
-
-function getStyle(feat, geom) {
-    return styles[feat.getGeometry().getType()];
-}
-
-function extend(coord) {
-    var a = [];
-    a.push(coord);
-    a.push([coord[0] - 10, coord[1]]);
-    a.push([coord[0] - 10, coord[1] - 10]);
-    a.push([coord[0], coord[1] - 10]);
-    a.push([coord[0], coord[1]]);
-    return a;
-}
-
-var view = new ol.View({
-    center: ol.proj.transform([-104.65, 38.25], 'EPSG:4326', 'EPSG:3857'),
-    zoom: getHash('zoom') || 15
-});
-
-var map = new ol.Map({
-    layers: layers,
-    target: 'map',
-    view: view
-});
-
-
-
-function drawFeat(feat, caller) {
-    if (typeof feat === 'string') {
-	feat = JSON.parse(feat);
-    }
-
-    var geom = feat.features[0].geometry.rings[0];
-    var poly = new ol.Feature(new ol.geom.MultiPoint(geom));
-
-    vectorSource.addFeature(poly);
-}
-
-function clicker(evt) {
-    document.getElementById('info').innerHTML = '';
-    setHash(evt.coordinate);
-    view.setCenter(evt.coordinate);
-    vectorSource.clear();
-
-    getInfo(evt.coordinate, function(data) {
-	fillTable(data);
-	drawFeat(data, "clicker");
-    });
-}
-
-map.on('singleclick', clicker);
-map.on('moveend', function(evt) {
-    setHash();
-});
-
-function getInfo(coord, fn) {
-    var jsonUrl = 'http://maps.co.pueblo.co.us/outside/rest/services/pueblo_county_parcels_bld_footprints/MapServer/2/query';
-    var jsonOpts = {
-	f: 'json',
-	returnGeometry: 'true',
-	spatialRel: 'esriSpatialRelIntersects',
-	geometry: JSON.stringify({
-	    rings: [extend(coord)],
-	    spatialReference: {
-		wkid: 102100,
-		latestWkid: 3857
-	    }
-	}),
-	geometryType: 'esriGeometryPolygon',
-	inSR: '102100',
-	outFields: '*',
-	outSR: '102100'
-    };
-
-    var a = jsonUrl + '?' + $.param(jsonOpts);
-
-
-    $.get(a, function(data) {
-	fn.call(null, JSON.parse(data));
-    });
-}
-
 function fillTable(feat) {
     $('#info').html('');
     var table = $('<table>');
 
     table.addClass('table-striped');
+    if (!feat.features) {
+	return;
+    }
 
     for (var namer in prettyMap) {
 	key = namer;
@@ -266,8 +134,8 @@ function fillTable(feat) {
 	    if (key.match(/^break\d/)) {
 		table.append($('<tr>').append($('<td colspan="2">').html("<b>" + val + "</b>")));
 	    } else {
-		if (key && feat.features[0].attributes[val]) {
-		    table.append($('<tr>').append($('<td>').html(key), $('<td>').html(feat.features[0].attributes[val])));
+		if (key && feat.features[0].properties[val]) {
+		    table.append($('<tr>').append($('<td>').html(key), $('<td>').html(feat.features[0].properties[val])));
 		}
 	    }
 	    break;
@@ -276,14 +144,14 @@ function fillTable(feat) {
 		var ii, ll, v = [];
 
 		for (ii = 0, ll = val.length; ii < ll; ii++) {
-		    var item = feat.features[0].attributes[val[ii]];
+		    var item = feat.features[0].properties[val[ii]];
 		    if (item) {
 			item = item.trim();
 		    } else {
 			item = "";
 		    }
 		    if (item !== "") {
-			v.push(feat.features[0].attributes[val[ii]]);
+			v.push(feat.features[0].properties[val[ii]]);
 		    }
 		}
 		val = v.join("<br />");
@@ -294,7 +162,7 @@ function fillTable(feat) {
 		var kk, vv;
 		for (kk in val) {
 		    vv = val[kk];
-		    var nvv = vv(key, feat.features[0].attributes[kk], feat.features[0].attributes);
+		    var nvv = vv(key, feat.features[0].properties[kk], feat.features[0].properties);
 		    if (key && nvv) {
 			table.append($('<tr>').append($('<td>').html(key), $('<td>').html(nvv)));
 		    }
@@ -312,66 +180,88 @@ function fillTable(feat) {
     $('#info').append(table);
 }
 
-function getHash(item) {
-    var ret;
-    var parts = parseHash();
-    if (parts) {
+var utfGrid = new L.UtfGrid('http://wms.deftly.net/utfgrid_water_wells/{z}/{x}/{y}.json?callback={cb}', {
+});
 
-	if (item === 'zoom') {
-	    ret = parts[0];
-	}
+var wellImg = L.tileLayer("http://wms.deftly.net/water_wells/{z}/{x}/{y}.png", {
+    maxZoom: 18,
+    minZoom: 16
+});
 
-	if (item === 'coords') {
-	    ret = parts[1];
-	}
-	return ret;
+/*var bl = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  id: 'examples.map-i875mjb7'
+  });*/
+var bl = new L.Google();
+
+var parcels = new L.esri.imageMapLayer("http://maps.co.pueblo.co.us/outside/rest/services/pueblo_county_parcels_bld_footprints/MapServer/export", {
+    maxZoom: 18,
+    minZoom: 16,
+    format: 'png32',
+    transparent: true,
+    noData: 0
+});
+
+var popupTemplate = "<h3>{PAR_NUM}</h3><small></small>";
+var wellTemplate = "<h3>{receipt}</h3><small>Permit: {permit}</small>";
+var log = true;
+
+utfGrid.on('mouseover', function (e) {
+    $('#well').html(prettyWell(e.data));
+});
+
+var map = L.map('map', {
+    center: [38.25, -104.65],
+    zoom: 13,
+    layers: [bl, parcels, wellImg]
+});
+
+var service = L.esri.Services.featureLayer({
+    url: 'http://maps.co.pueblo.co.us/outside/rest/services/pueblo_county_parcels_bld_footprints/MapServer/2'
+});
+
+var geoL = L.geoJson(null, {
+    style: function() {
+	return {"color": "red"};
     }
-}
+}).addTo(map);
 
-function setHash(coords, zoom) {
-    var c = coords || view.getCenter();
-    var z = zoom || view.getZoom();
-    window.location.hash = "/" + z + '/' + c.join('/');
-}
+map.on('click', function(e) {
+    geoL.clearLayers();
 
-function parseHash(initial) {
-    var parts = window.location.hash.split('/');
-    if (parts.length > 3) {
-	parts.shift(); // clobber the #
+    service.query().nearby(e.latlng).run(function(error, featureCollection, response){
+	geoL.addData(featureCollection);
+	fillTable(featureCollection);
+    });
+});
 
-	var z = parts.shift();
-	var coords = parts;
-	if (initial) {
-	    getInfo(coords, function(data) {
-		fillTable(data);
-		drawFeat(data, "parsehash");
-	    });
-	} else {
-	    return [z, coords];
-	}
-    }
-}
+
+map.addLayer(utfGrid);
+
+var baseMaps = {
+    "Google": bl
+};
+
+var overlayMaps = {
+    "Wells": wellImg,
+    "Parcels": parcels
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 function doSearch() {
     var address = $('#search').val();
+    $('#well').html("");
     geocoder.geocode({'address': address}, function(results, status) {
 	if (results.length > 0) {
 	    var lng = results[0].geometry.location.lng();
 	    var lat = results[0].geometry.location.lat();
-	    var coord = [lng, lat];
-	    coord = ol.proj.transform(coord, 'EPSG:4326', 'EPSG:3857');
-
-	    vectorSource.clear();
-	    vectorSource.addFeature(new ol.Feature(new ol.geom.Point(coord)));
-
-	    view.setCenter(coord);
-	    view.setZoom(18);
-
-	    setHash(coord, 18);
-
-	    getInfo(coord, function(data) {
-		fillTable(data);
-		drawFeat(data, "dosearch");
+	    map.panTo([lat, lng]);
+	    map.setZoom(17);
+	    service.query().contains(L.latLng(lat, lng)).run(function(error, featureCollection, response){
+		geoL.addData(featureCollection);
+		fillTable(featureCollection);
 	    });
 	}
     });
@@ -384,40 +274,4 @@ $('#search').keyup(function(e) {
     }
 });
 
-parseHash(true);
-
-$('#layer-select').change(function() {
-    var style = $(this).find(':selected').val();
-    var i, ii;
-    for (i = 0, ii = layers.length; i < ii; ++i) {
-	var ls = layers[i].get('style');
-	if (ls != 'AlwaysShow') {
-	    layers[i].set('visible', (layers[i].get('style') == style));
-	}
-    }
-});
-
-$('#layer-select').trigger('change');
-
-var mapElement = document.getElementById('map');
-
-var displayCountryInfo = function(coordinate) {
-    var viewResolution = /** @type {number} */ (view.getResolution());
-    utfGridSource.forDataAtCoordinateAndResolution(coordinate, viewResolution, function(data) {
-	// If you want to use the template from the TileJSON,
-	//  load the mustache.js library separately and call
-	//  info.innerHTML = Mustache.render(gridSource.getTemplate(), data);
-	mapElement.style.cursor = data ? 'pointer' : '';
-	if (data) {
-	    console.log(data);
-	}
-    });
-};
-
-map.on('pointermove', function(evt) {
-    if (evt.dragging) {
-	return;
-    }
-    var coordinate = map.getEventCoordinate(evt.originalEvent);
-    displayCountryInfo(coordinate);
-});
+var hash = new L.Hash(map);
